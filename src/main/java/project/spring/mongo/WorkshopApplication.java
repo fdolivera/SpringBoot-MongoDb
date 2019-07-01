6ef7152009c6552d4c6b372cbb7af81d7f902461
Felipe Oliveira <felipe.olivera_@outlook.com>
2019-07-01 20:55:32 -0300

    Created project

---
+++ src/main/java/project/spring/mongo/WorkshopApplication.java
@@ -0,0 +1,13 @@
+package project.spring.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopApplication.class, args);
+	}
+
+}
